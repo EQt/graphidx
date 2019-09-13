339cc4b8ada59b9dd75fc6c1b6a7ea391b1dd841
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-13 15:48:47 +0200

    start independent python module

---
+++ python/_graphidx.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>                // for std::sort
+#include <pybind11/pybind11.h>
+
+#include "utils/compiler.hpp"
+#include "py_np.hpp"
+
+namespace py = pybind11;
+
+
+void reg_order(py::module &m);
+void reg_idx(py::module &m);
+void reg_spanning(py::module &m);
+
+
+/**
+   For testing: create an array with 3 elements
+ */
+py::array_t<double>
+_test_create_array()
+{
+    double *x = new double[3];
+    x[0] = 13.0;
+    x[1] = -1.0;
+    x[2] = 42.0;
+    return py::array_t<double>({3}, {sizeof(double)}, x);
+}
+
+
+PYBIND11_MODULE(_treelas, m)
+{
+    m.doc() = R"pbdoc(
+        Access the C++ implementation of GraphIdx
+        -----------------------------------------
+
+        https://github.com/eqt/treelas
+
+        .. currentmodule:: _graphidx
+
+        .. autosummary::
+           :toctree: _generate
+
+    )pbdoc";
+    m.attr("__author__") = "Elias Kuthe <elias.kuthe@tu-dortmund.de>";
+    m.attr("__compiler__") = compiler_info();
+
+    m.def("_stdcxx_sort", [](py::array_f64 a) -> void {
+        std::sort(a.mutable_data(), a.mutable_data()+a.size());
+      },
+      R"pbdoc(
+        std::sort from libstdc++ (to be compared against numpy.sort)
+      )pbdoc");
+
+    reg_order(m);
+    reg_idx(m);
+    reg_spanning(m);
+}
