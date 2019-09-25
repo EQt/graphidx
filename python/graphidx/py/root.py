4546edd79a44abbf4b92267d2223818be3658494
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-25 11:38:18 +0200

    usage example

--- python/graphidx/py/root.py
+++ python/graphidx/py/root.py
@@ -1,9 +1,15 @@
 from numba import njit
+import numpy as np
 
 
 @njit(cache=True)
 def find_root(parent):
-    """Return the smallest i with parent[i] == i, or -1 if none exists"""
+    """
+    Return the smallest i with parent[i] == i, or -1 if none exists
+
+    >>> find_root(np.array([1, 2, 0, 3]))
+    3
+    """
     for i, p in enumerate(parent):
         if i == p:
             return i
