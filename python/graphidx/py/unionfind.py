e3ae0378ae8910cb759817e22633379c3a27d264
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-25 11:41:49 +0200

    add usage example to the top

--- python/graphidx/py/unionfind.py
+++ python/graphidx/py/unionfind.py
@@ -1,18 +1,25 @@
+"""
+Classical Union-Find data structure with path compression as described
+e.g. in CLRS in Chapter 21 "Disjooint Sets" (3rd edition).
+
+>>> u = UnionFind(3)
+
+>>> u.find(1) == u.find(2)
+False
+"""
 import numpy as np
 import numba
 
 
+@numba.njit(cache=True)
+def _uf_find(pi, x):
+    if pi[x] != x:
+        pi[x] = _uf_find(pi, pi[x])
+    return pi[x]
+
+
 @numba.jitclass([('p', numba.uint64[:]), ('rank', numba.uint64[:])])
 class UnionFind:
-    """
-    Classical Union-Find data structure with path compression as described
-    e.g. in CLRS in Chapter 21 "Disjooint Sets" (3rd edition).
-
-    >>> u = UnionFind(2);
-
-    >>> u.find(1) == u.find(2)
-    False
-    """
     def __init__(self, n: int):
         self.p = np.empty(n, dtype=np.uint64)
         self.rank = np.empty(n, dtype=np.uint64)
@@ -26,6 +33,6 @@ class UnionFind:
             self.rank[i] = 0
 
     def find(self, x: int):
-        if self.p[x] != x:
-            self.p[x] = self.find(self.p[x])
-        return self.p[x]
+        return _uf_find(self.p, x)
+
+
