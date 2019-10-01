898d33eeb7ee8a9881b5e0e398893a500e8e0594
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-10-01 19:03:15 +0200

    update emacs local variables

--- cxx/.dir-locals.el
+++ cxx/.dir-locals.el
@@ -2,8 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c++-mode
-   ;; (c-offsets-alist . (inextern-lang . 0))
-   (compile-command . "make -C ../build/ pytest")
+   (compile-command . "cd ../build/ && make doctests -j$(nproc) && ./doctests -nc")
    (c-file-style . "stroustrup")))
 
 
