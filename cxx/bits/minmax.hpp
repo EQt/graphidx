f951722afe0770977a7b29d79403ce111c71024f
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-10-04 11:58:50 +0200

    remove deprecated function clip

--- cxx/bits/minmax.hpp
+++ cxx/bits/minmax.hpp
@@ -16,19 +16,3 @@ find_minmax(const float_ *y,
     y_min = *std::min_element(y, y + n);
     y_max = *std::max_element(y, y + n);
 }
-
-
-template<typename float_ = float>
-[[deprecated]]
-inline float_
-clip(float_ x, float_ a, float_ b)
-{
-    /*
-    if (x >= b)
-        return b;
-    if (x <= a)
-        return a;
-    return x;
-    */
-    return x >= b ? b : (x <= a ? a : x);
-}
