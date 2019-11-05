78c47ffd01bca2b58afa5ca5917152d9f7d2caea
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-05 21:12:32 +0100

    provide weights::UnitWeights

--- rust/src/weights.rs
+++ rust/src/weights.rs
@@ -11,9 +11,50 @@
 //! [`ConstantWeights`]: struct.ConstantWeights.html
 //! [`ArrayWeights`]: struct.ConstantWeights.html
 use std::ops::Index;
+// use num_traits::identities::{One, one};
 
 pub trait Weighted<T>: Index<usize, Output = T> {
     fn len(&self) -> usize;
+}
+
+/// Weight 1.0 for every element
+#[derive(PartialEq, Debug)]
+pub struct UnitWeights<T> {
+    _c: std::marker::PhantomData<T>,
+}
+
+impl Weighted<f64> for UnitWeights<f64> {
+    fn len(&self) -> usize {
+        std::usize::MAX
+    }
+}
+
+impl Index<usize> for UnitWeights<f64> {
+    type Output = f64;
+    fn index(&self, _i: usize) -> &Self::Output {
+        &1f64
+    }
+}
+
+impl Weighted<f32> for UnitWeights<f32> {
+    fn len(&self) -> usize {
+        std::usize::MAX
+    }
+}
+
+impl Index<usize> for UnitWeights<f32> {
+    type Output = f32;
+    fn index(&self, _i: usize) -> &Self::Output {
+        &1f32
+    }
+}
+
+impl<T> UnitWeights<T> {
+    pub fn new() -> Self {
+        UnitWeights {
+            _c: Default::default(),
+        }
+    }
 }
 
 /// Same weight for every element.
@@ -68,7 +109,7 @@ impl<T> ArrayWeights<T> {
 
 #[cfg(test)]
 mod tests {
-    use super::{ConstantWeights, ArrayWeights};
+    use super::{ArrayWeights, ConstantWeights, UnitWeights};
 
     #[test]
     fn constant_weights_13() {
@@ -76,6 +117,14 @@ mod tests {
         assert_eq!(w[5], 13.5);
         assert_eq!(w[0], 13.5);
         assert_eq!(w[13], 13.5);
+    }
+
+    #[test]
+    fn unit_weights_13() {
+        let w = UnitWeights::<f64>::new();
+        assert_eq!(w[5], 1.0);
+        assert_eq!(w[0], 1.0);
+        assert_eq!(w[13], 1.0);
     }
 
     #[test]
