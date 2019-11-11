d5b8d2c15e262ed4c5e4758b10a0a66e827203f7
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-11 15:42:28 +0100

    Io.prettyarr

--- julia/src/Io.jl
+++ julia/src/Io.jl
@@ -3,6 +3,18 @@ Input/output
 """
 module Io
 
+"""
+    prettyarr(a::Array)
+
+Get a string represantation of an array similar to the REPL output
+"""
+function prettyarr(a::Array)::String
+    buf = IOBuffer()
+    show(buf, MIME("text/plain"), a)
+    read(seekstart(buf), String)
+end
+
+
 include("Io/dimacs.jl")
 
 end
