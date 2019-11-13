1c0b77aaf58880f511d05c63fc47b40ecf20559b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-13 13:43:26 +0100

    new syntax in python

--- python/graphidx/weights.py
+++ python/graphidx/weights.py
@@ -10,30 +10,37 @@ class Weights:
         """
         if isinstance(x, Weights):
             return x
+        elif isinstance(x, type):
+            return Ones(x)
         elif isinstance(x, (float, int)):
-            return ConstantWeights(x)
+            return Const(x)
         elif hasattr(x, '__getitem__'):
-            return ArrayWeights(x)
+            return Array(x)
         raise ValueError(type(x))
 
+    def __call__(self, i):
+        return self[i]
 
-class ConstantWeights(Weights):
+
+class Ones(Weights):
+    def __init__(self, dtype=float):
+        self.dtype = dtype
+
+    def __getitem__(self, _):
+        return dtype(1)
+
+
+class Const(Weights):
     def __init__(self, c: float):
         self.c = c
 
     def __getitem__(self, _):
         return self.c
 
-    def __call__(self, _):
-        return self.c
 
-
-class ArrayWeights(Weights):
+class Array(Weights):
     def __init__(self, a):
         self.a = a
 
     def __getitem__(self, i):
-        return self.a[i]
-
-    def __call__(self, i):
         return self.a[i]
