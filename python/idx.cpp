90556db41fc73eda19eac893dbe13c006baa04e1
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-02-26 15:34:58 +0100

    GridGraph.shape property

--- python/idx.cpp
+++ python/idx.cpp
@@ -54,6 +54,9 @@ reg_idx(py::module &m)
         .def("num_nodes", &GridGraph::num_nodes)
         .def_property_readonly("n1", [](const GridGraph &grid){ return grid.n1; })
         .def_property_readonly("n2", [](const GridGraph &grid){ return grid.n2; })
+        .def_property_readonly("shape", [](const GridGraph &g){
+                                            return py::make_tuple(g.n1, g.n2);
+                                        })
         .def("__repr__", [](const GridGraph &g){
                              return std::string("GridGraph(") +
                                  std::to_string(g.n1) + ", " + std::to_string(g.n2) + ")";
--- python/test/test_grid.py
+++ python/test/test_grid.py
@@ -6,6 +6,7 @@ def test_grid3x2():
     grid = GridGraph(3, 2)
     assert repr(grid) == "GridGraph(3, 2)"
     assert grid.num_nodes() == 6
+    assert grid.shape == (3, 2)
 
 
 def test_grid3x2_idx():
