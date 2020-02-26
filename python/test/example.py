95207c4ff4d70f09e1d825362bb5facc161fcb30
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-02-26 18:30:59 +0100

    fix imports

--- python/test/example.py
+++ python/test/example.py
@@ -2,9 +2,11 @@ from urllib.request import urlretrieve
 from os import path
 from graphidx._graphidx import io as gio
 from graphidx.tree import ChildrenIndex
+import graphidx
 
 
-url = "https://www.cc.gatech.edu/dimacs10/archive/data/streets/belgium.osm.graph.bz2"
+url = "https://www.cc.gatech.edu/dimacs10/archive/data/streets/" + \
+    "belgium.osm.graph.bz2"
 fname = "belgium.bz2"
 if not path.exists(fname):
     print("downloading", url)
