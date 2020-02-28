cf54d774c99661a2d0c4f226ec01b8616e56146e
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-02-28 18:09:31 +0100

    extract method

--- python/test/test_bfs.py
+++ python/test/test_bfs.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 
+def reorder_parent(parent: np.ndarray, order: np.ndarray) -> np.ndarray:
+    iorder = np.argsort(order)
+    return iorder[parent[order]]
+
+
 def test_reversed_bfs_pi():
     """
     Check that the transformed parent is consistent
@@ -8,5 +13,4 @@ def test_reversed_bfs_pi():
     parent = np.array([0, 0, 1, 2, 3, 0, 7, 8, 3, 8])
     bfs = np.array([6, 9, 7, 8, 4, 3, 2, 5, 1, 0])
     pi = np.array([2, 3, 3, 5, 5, 6, 8, 9, 9, 9])
-    ibfs = np.argsort(bfs)
-    assert (ibfs[parent[bfs]] == pi).all()
+    assert all(reorder_parent(parent, bfs) == pi)
