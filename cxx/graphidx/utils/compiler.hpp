bb4620ee525ee51f3872d439cd242a3c4b2ce823
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-03-10 17:19:54 +0100

    check whether compiled with address sanatizer

--- cxx/graphidx/utils/compiler.hpp
+++ cxx/graphidx/utils/compiler.hpp
@@ -2,6 +2,10 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
+#ifndef __has_feature
+#  define __has_feature(x) false
+#endif
+
 
 /** information about compilation */
 constexpr const char*
@@ -17,4 +21,15 @@ compiler_info()
 #endif
      " on "  __DATE__  " " __TIME__
          ;
+}
+
+
+constexpr bool
+asan_enabled()
+{
+#if __has_feature(address_sanitizer) || (defined(__SANITIZE_ADDRESS__) && __SANITIZE_ADDRESS__)
+        return true;
+#else
+        return false;
+#endif
 }
