f15787edd7bfdee64e148def18ecf639208efa40
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-03-13 12:44:31 +0100

    only create the associated streambuf

--- cxx/graphidx/io/autoistream.hpp
+++ cxx/graphidx/io/autoistream.hpp
@@ -7,23 +7,24 @@
 #include "magic.hpp"
 
 
-inline std::istream*
-make_istream(const char *fname)
+inline std::streambuf*
+make_sbuf(const char *fname)
 {
-    if (is_gzip(fname))
-        return new GZIStream(fname);
+    if (is_gzip(fname)) {
+        return &(new GZIStreamBuf())->open(fname);
+    }
     if (is_bzip2(fname))
-        return new BZ2IStream(fname);
-    return new std::ifstream(fname);
+        return &(new BZ2IStreamBuf())->open(fname);
+    return (new std::filebuf)->open(fname, std::ios_base::in);
 }
 
 
 class AutoIStream : public std::istream
 {
-    std::unique_ptr<std::istream> io;
+    std::unique_ptr<std::streambuf> io;
 
 public:
-    AutoIStream(const char *fname) : io(make_istream(fname)) {
-        rdbuf(io.get()->rdbuf());
+    AutoIStream(const char *fname) : std::istream(nullptr), io(make_sbuf(fname)) {
+        rdbuf(io.get());
     }
 };
