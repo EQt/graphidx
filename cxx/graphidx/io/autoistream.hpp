72f9cc0f2f505267941c3e6f944715f8c6ebc5c6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-03-13 13:58:05 +0100

    make it compile when gzip is not present

--- cxx/graphidx/io/autoistream.hpp
+++ cxx/graphidx/io/autoistream.hpp
@@ -2,19 +2,33 @@
 #include <fstream>
 #include <memory>
 
-#include "bz2istream.hpp"
-#include "gzistream.hpp"
 #include "magic.hpp"
+#if __has_include(<bzlib.h>)
+#  include "bz2istream.hpp"
+#endif
+#if __has_include(<zlib.h>)
+#  include "gzistream.hpp"
+#endif
 
 
 inline std::streambuf*
 make_sbuf(const char *fname)
 {
     if (is_gzip(fname)) {
+#if __has_include(<zlib.h>)
         return &(new GZIStreamBuf())->open(fname);
+#else
+        throw std::invalid_argument("Not compiled with GZIP");
+#endif
+
     }
-    if (is_bzip2(fname))
+    if (is_bzip2(fname)) {
+#if __has_include(<bzlib.h>)
         return &(new BZ2IStreamBuf())->open(fname);
+#else
+        throw std::invalid_argument("Not compiled with BZIP2");
+#endif
+    }
     return (new std::filebuf)->open(fname, std::ios_base::in);
 }
 
