0980085d2575b38627565882db3c3a5899eb357c
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-07-18 15:01:58 +0200

    playing around with localeconv

--- julia/src/Utils/thousand.jl
+++ julia/src/Utils/thousand.jl
@@ -3,8 +3,8 @@ const LC_NUMERIC = 1
 const LC_ALL = 6
 
 
-function setlocale(l="en_US", lc=LC_NUMERIC)
-    path = ccall(:setlocale, Cstring, (Cint, Cstring), LC_ALL, l)
+function setlocale(l="en_US.utf8", lc=LC_NUMERIC)
+    path = ccall(:setlocale, Cstring, (Cint, Cstring), lc, l)
     @assert path != C_NULL
     unsafe_string(path)
 end
@@ -14,6 +14,23 @@ struct Lconv
     decimal_point::Ptr{Cchar}
     thousands_sep::Ptr{Cchar}
     grouping::Ptr{Cchar}
+end
+
+
+function Lconv(s::String = ".\0_\0\3\3\0")
+    fcount = 3  # field count
+    @assert count(r"\0", s) == fcount
+    @assert s[end] == '\0'
+    ms = ccall(:malloc, Ptr{Cchar}, (Cint,), length(s))
+    unsafe_copyto!(ms, typeof(ms)(pointer(s)), length(s))
+    Lconv((ms + u.offset for u in split(s, '\0')[1:fcount])...)
+end
+
+
+function setlconv(l::Lconv)
+    error("setlconv does not work")
+    lc = ccall(:localeconv, Ptr{Lconv}, ())
+    unsafe_store!(lc, l)
 end
 
 
@@ -33,7 +50,6 @@ function Base.show(io::IO, l::Lconv)
     end
     println(io, "}")
 end
-
 
 
 function fmt_thousands(d::Integer)
