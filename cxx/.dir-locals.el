fa781956b3ae4161f7074baba77b2c9c3a5e001d
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-10-26 14:41:08 +0100

    set color env variable

--- cxx/.dir-locals.el
+++ cxx/.dir-locals.el
@@ -2,7 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c++-mode
-   (compile-command . "cd ../../build/ && make doctests -j$(nproc) && ./doctests -nc")
+   (compile-command . "cd ../../build/ && COLOR=0 make doctests -j$(nproc) && ./doctests -nc")
    (c-file-style . "stroustrup")))
 
 
