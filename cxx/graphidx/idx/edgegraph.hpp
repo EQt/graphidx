fa54b2800aae330c63f740c574f215c2be0f222b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2021-01-09 14:24:39 +0100

    typos

--- cxx/graphidx/idx/edgegraph.hpp
+++ cxx/graphidx/idx/edgegraph.hpp
@@ -8,11 +8,11 @@ struct EdgeGraph
     std::vector<int> head;
     std::vector<int> tail;
 
-    inline size_ num_nodes() const {
+    inline size_t num_nodes() const {
         return n;
     }
 
-    inline size num_edges() const {
-        retrun head.size();
+    inline size_t num_edges() const {
+        return head.size();
     }
 };
