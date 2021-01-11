c7a136c1d417bfcd9e8a55e37839caf21f205d7b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2021-01-11 16:13:53 +0100

    fixed issue in Prim's mst implementation (C++)

--- cxx/graphidx/spanning/prim_mst.hpp
+++ cxx/graphidx/spanning/prim_mst.hpp
@@ -13,6 +13,57 @@ template <
     typename int_t = int,
     typename Queue = Heap<Tag, int_t, float_t>>
 void
+prim_mst_dbg(
+    int_t *parent,
+    const float_t *edge_weight,
+    const IncidenceIndex<int_t> &idx,
+    const int_t root,
+    std::ostream &cerr = std::cerr)
+{
+    Queue queue(idx.num_nodes());
+    for (size_t i = 0; i < idx.num_nodes(); i++)
+        parent[i] = ~0;
+
+    queue.push({root, std::numeric_limits<float_t>::min()});
+    parent[root] = ~root;
+    while (!queue.empty()) {
+        const auto u = queue.top();
+        cerr << "fin: " << u << " pi = " << ~parent[u] << " was " << queue[u]
+             << std::endl;
+        queue.pop();
+        parent[u] = ~parent[u];
+        for (const auto [v, eidx] : idx[u]) {
+            if (parent[v] >= 0) {
+                cerr << "chk: " << v << " finished" << std::endl;
+                continue; // already finished?
+            }
+            if (!queue.contains(v)) {
+                parent[v] = ~u;
+                queue.push({v, edge_weight[eidx]});
+                cerr << "new: " << v << " --> " << edge_weight[eidx]
+                     << " pi = " << ~parent[v] << std::endl;
+            } else {
+                if (edge_weight[eidx] < queue[v]) {
+                    const auto was = queue[v];
+                    parent[v] = ~u;
+                    queue.decrease(v, edge_weight[eidx]);
+                    cerr << "upd: " << v << " --> " << edge_weight[eidx] << " was "
+                         << was << std::endl;
+                } else {
+                    cerr << "chk: " << v << " no upd" << std::endl;
+                }
+            }
+        }
+    }
+}
+
+
+template <
+    typename Tag,
+    typename float_t,
+    typename int_t = int,
+    typename Queue = Heap<Tag, int_t, float_t>>
+void
 prim_mst_edges(
     int_t *parent,
     const float_t *edge_weight,
@@ -27,8 +78,6 @@ prim_mst_edges(
     parent[root] = -root;
     while (!queue.empty()) {
         const auto u = queue.top();
-        std::cerr << "fin: " << u << " pi = " << -parent[u] << " was " << queue[u]
-                  << std::endl;
         queue.pop();
         parent[u] *= -1;
         for (const auto [v, eidx] : idx[u]) {
@@ -37,13 +86,9 @@ prim_mst_edges(
             if (!queue.contains(v)) {
                 parent[v] = -u;
                 queue.push({v, edge_weight[eidx]});
-                std::cerr << "new: " << v << " --> " << edge_weight[eidx] << std::endl;
             } else if (edge_weight[eidx] < queue[v]) {
-                const auto was = queue[v];
                 parent[v] = -u;
                 queue.decrease(v, edge_weight[eidx]);
-                std::cerr << "upd: " << v << " --> " << edge_weight[eidx] << " was "
-                          << was << std::endl;
             }
         }
     }
@@ -70,5 +115,5 @@ prim_mst_edges(
 
 
 // Local Variables:
-// compile-command: "cd ../../../build && COLOR=0 make spantree &&
-// ./cxx/benchmark/spantree" End:
+// compile-command: "cd ../../../build/ && COLOR=0 make doctests && ./doctests -nc"
+// End:
