40c36bf9b8b562d982ea9e7b6050810844addb81
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2021-01-18 18:30:29 +0100

    outsource test data

---
+++ cxx/test/test_mst37.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+static const char *MST_37 =
+R"(fin: 0 pi = 0 was 2.22507e-308
+new: 1 --> -0 pi = 0
+new: 3 --> -0.11 pi = 0
+fin: 3 pi = 0 was -0.11
+new: 4 --> -1 pi = 3
+chk: 0 finished
+new: 6 --> -0.02 pi = 3
+fin: 4 pi = 3 was -1
+chk: 3 finished
+new: 5 --> -1.41 pi = 4
+upd: 1 --> -1.11 was -0
+new: 7 --> -0.78 pi = 4
+fin: 5 pi = 4 was -1.41
+chk: 4 finished
+new: 2 --> -0.58 pi = 5
+new: 8 --> -0.8 pi = 5
+fin: 1 pi = 4 was -1.11
+chk: 0 finished
+chk: 2 no upd
+chk: 4 finished
+fin: 8 pi = 5 was -0.8
+upd: 7 --> -1.43 was -0.78
+chk: 5 finished
+new: 11 --> -1.43 pi = 8
+fin: 7 pi = 8 was -1.43
+upd: 6 --> -0.24 was -0.02
+chk: 8 finished
+chk: 4 finished
+new: 10 --> -0.51 pi = 7
+fin: 11 pi = 8 was -1.43
+chk: 10 no upd
+chk: 8 finished
+new: 14 --> -0.13 pi = 11
+fin: 2 pi = 5 was -0.58
+chk: 1 finished
+chk: 5 finished
+fin: 10 pi = 7 was -0.51
+new: 9 --> -0.04 pi = 10
+chk: 11 finished
+chk: 7 finished
+new: 13 --> -0.14 pi = 10
+fin: 6 pi = 7 was -0.24
+chk: 7 finished
+chk: 3 finished
+upd: 9 --> -0.79 was -0.04
+fin: 9 pi = 6 was -0.79
+chk: 10 finished
+chk: 6 finished
+new: 12 --> -0.33 pi = 9
+fin: 12 pi = 9 was -0.33
+upd: 13 --> -0.43 was -0.14
+chk: 9 finished
+new: 15 --> -0.74 pi = 12
+fin: 15 pi = 12 was -0.74
+new: 16 --> -0.73 pi = 15
+chk: 12 finished
+new: 18 --> -0.65 pi = 15
+fin: 16 pi = 15 was -0.73
+chk: 15 finished
+new: 17 --> -1.14 pi = 16
+upd: 13 --> -0.44 was -0.43
+new: 19 --> -0.78 pi = 16
+fin: 17 pi = 16 was -1.14
+chk: 16 finished
+upd: 14 --> -1.06 was -0.13
+new: 20 --> -0.38 pi = 17
+fin: 14 pi = 17 was -1.06
+upd: 13 --> -0.52 was -0.44
+chk: 11 finished
+chk: 17 finished
+fin: 19 pi = 16 was -0.78
+upd: 18 --> -0.7 was -0.65
+chk: 20 no upd
+chk: 16 finished
+fin: 18 pi = 19 was -0.7
+chk: 19 finished
+chk: 15 finished
+fin: 13 pi = 14 was -0.52
+chk: 12 finished
+chk: 14 finished
+chk: 10 finished
+chk: 16 finished
+fin: 20 pi = 17 was -0.38
+chk: 19 finished
+chk: 17 finished
+)";
