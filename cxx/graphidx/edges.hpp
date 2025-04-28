faaf309f03aeaf6bb52c3fc2f9ba6118b011986a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2025-04-28 16:06:28 +0200

    fix: prefer larger type for iteration

--- cxx/graphidx/edges.hpp
+++ cxx/graphidx/edges.hpp
@@ -3,16 +3,16 @@
  */
 #pragma once
 #include <functional>
-#include <graphidx/idx/incidence.hpp>
 #include <graphidx/idx/biadjacent.hpp>
+#include <graphidx/idx/incidence.hpp>
 
 
 template <typename int_t>
 void
 edges(const IncidenceIndex<int_t> &idx, std::function<void(int_t, int_t, int_t)> f)
 {
-    for (int_t u = 0; u < (int_t)idx.num_nodes(); u++) {
-        for (const auto [v, ei] : idx[u]) {
+    for (size_t u = 0; u < idx.num_nodes(); u++) {
+        for (const auto &[v, ei] : idx[u]) {
             f(u, v, ei);
         }
     }
@@ -23,7 +23,7 @@ template <typename int_t>
 void
 edges(const BiAdjacentIndex<int_t> &idx, std::function<void(int_t, int_t)> f)
 {
-    for (int_t v = 0; v < (int_t)idx.num_nodes(); v++) {
+    for (size_t v = 0; v < idx.num_nodes(); v++) {
         for (int_t u : idx[v]) {
             f(v, u);
         }
